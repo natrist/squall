ef7507727be75c4da98971e395b941249a579e88
superp00t <superp00t@tutanota.com>
2023-08-04 20:44:16 -0400

    chore(memory): rely on external dependency for allocation functions

--- storm/Memory.hpp
+++ storm/Memory.hpp
@@ -1,15 +1,6 @@
 #ifndef STORM_MEMORY_HPP
 #define STORM_MEMORY_HPP
 
-#include <cstdint>
-#include <cstdlib>
-
-void* SMemAlloc(size_t bytes, const char* filename, int32_t linenumber, uint32_t flags);
-
-void SMemFree(void* ptr);
-
-void SMemFree(void* ptr, const char* filename, int32_t linenumber, uint32_t flags);
-
-void* SMemReAlloc(void* ptr, size_t bytes, const char* filename, int32_t linenumber, uint32_t flags);
+#include <mem/Memory.hpp>
 
 #endif
