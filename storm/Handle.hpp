3c97ee30839a39625e98e45374aa4c80b9df50fe
VDm <vdm-dev@users.noreply.github.com>
2024-02-21 01:18:06 +0400

    chore(log): remove redundant Common.hpp

--- storm/Handle.hpp
+++ storm/Handle.hpp
@@ -7,4 +7,18 @@
     typedef struct name##__* name
 #endif
 
+#if !defined(DECLARE_STRICT_HANDLE)
+#define DECLARE_STRICT_HANDLE(name) \
+    typedef struct name##__ {       \
+        int unused;                 \
+    }* name
+#endif
+
+#if !defined(DECLARE_DERIVED_HANDLE)
+#define DECLARE_DERIVED_HANDLE(name, base)      \
+    typedef struct name##__ : public base##__ { \
+        int unused;                             \
+    }* name
+#endif
+
 #endif
